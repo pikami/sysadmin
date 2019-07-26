070b795b9f436752726a3bb74d5346e1f2d23f84
Pijus Kamandulis <pikami@users.noreply.github.com>
2019-07-26 23:27:20 +0300

    Update renew-ssl.sh

--- security/renew-ssl.sh
+++ security/renew-ssl.sh
@@ -1,6 +1,12 @@
+# Get certs
 acme.sh --issue -d pikami.org -d mail.pikami.org --webroot /var/www/html/ --force
+# Install cert nginx
 acme.sh --install-cert -d pikami.org \
   --key-file       /etc/ssl/private/domain.key \
   --fullchain-file /etc/ssl/certs/chained.pem
+# Install cert postfix
 cp /etc/ssl/certs/chained.pem /etc/postfix/chained.pem
 cp /etc/ssl/private/domain.key /etc/postfix/domain.key
+# Reload services
+service nginx reload
+service postfix reload
